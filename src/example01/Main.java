d208ce85e7b33c5bc355e1ec80e8f7e4a054d5e6
david <gnshjoo@gmail.com>
2019-10-02 21:00:25 +0900

    first commit

---
+++ src/example01/Main.java
@@ -0,0 +1,14 @@
+package example01;
+
+/*
+작성자 : David
+작성일 : 2019.10.2
+* */
+
+// 클래스 선언부
+public class Main {
+    // 메소드 선언부
+    public static void main(String[] args) {
+        System.out.println("Hello, JAVA");
+    }
+}
---
+++ src/example02/Main.java
@@ -0,0 +1,10 @@
+package example02;
+
+public class Main {
+    public static void main(String[] args){
+        int x = 1;
+        int y = 2;
+        int result = x + y;
+        System.out.println(result);
+    }
+}
